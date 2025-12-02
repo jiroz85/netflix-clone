7fdb56b7f4cbc7ac7a4a5cfebab4bf309569d41c
jiroz85 <jiregnameseret9@gmail.com>
2025-12-02 04:26:02 -0800

    Initial commit with Netflix clone project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./components/styles/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/login/LoginPage.jsx";
 

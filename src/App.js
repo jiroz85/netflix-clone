f5057df15d0e178c00080028d661b6548417637e
jiroz85 <jiregnameseret9@gmail.com>
2025-12-02 05:04:39 -0800

    Trigger new deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./components/styles/App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.jsx";
 import LoginPage from "./pages/login/LoginPage.jsx";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
